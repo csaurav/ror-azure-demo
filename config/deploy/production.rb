b2bfecf1b212d7e4daaa46b6cc76ea2efd7a6f01
Saurav <csaurav80@gmail.com>
2016-03-07 12:29:10 +0000

    deploy script changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,8 @@ set :branch, "master"
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 # specify server DNS name, user & roles
-ask(:password, nil, echo:false) 
-  server 'vm-1-372ezf46.cloudapp.net', user: 'saurav', port: 22, password: fetch(:password), roles: %w{web app db}, primary: true
+ask(:password, nil) 
+  server 'vm-1-372ezf46.cloudapp.net', user: 'saurav', port: 22, password: fetch(:password), roles: %w{web app db}
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
